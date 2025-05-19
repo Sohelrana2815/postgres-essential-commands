5c35276c9f8ca9de9c578c75a4463af02003fa7e
Sohelrana2815 <sohel152302@gmail.com>
2025-05-19 11:49:13 +0600

    handling null vlaue

--- select.sql
+++ select.sql
@@ -1,12 +1,41 @@
 CREATE TABLE students ( student_id SERIAL PRIMARY KEY, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, age INT, grade CHAR(2), course VARCHAR(50), email VARCHAR(100), dob DATE, blood_group VARCHAR(5), country VARCHAR(50) );
-INSERT INTO students (first_name, last_name, age, grade, course, email, dob, blood_group, country) VALUES ('Alice', 'Smith', 21, 'B', 'Computer Science', 'alice.smith@example.com', '2004-05-19', 'B+', 'Canada'), ('Bob', 'Johnson', 20, 'A+', 'Chemistry', 'bob.johnson@example.com', '2005-01-27', 'O-', 'China'), ('Charlie', 'Williams', 23, 'C+', 'English', 'charlie.williams@example.com', '2002-04-08', 'AB+', 'USA'), ('David', 'Brown', 24, 'B+', 'Sociology', 'david.brown@example.com', '2001-08-12', 'A-', 'Brazil'), ('Emily', 'Jones', 22, 'A', 'History', 'emily.jones@example.com', '2003-07-11', 'B+', 'UK'), ('Fiona', 'Garcia', 23, 'B+', 'Physics', 'fiona.garcia@example.com', '2002-05-22', 'O-', 'Japan'), ('George', 'Miller', 22, 'C', 'Economics', 'george.miller@example.com', '2003-06-27', 'B+', 'France'), ('Hannah', 'Davis', 24, 'C+', 'Biology', 'hannah.davis@example.com', '2001-04-09', 'A+', 'Germany'), ('Isaac', 'Rodriguez', 20, 'A', 'Mathematics', 'isaac.rodriguez@example.com', '2005-03-04', 'O+', 'Australia'), ('Jane', 'Wilson', 20, 'D', 'Computer Science', 'jane.wilson@example.com', '2005-08-07', 'AB-', 'India')
+INSERT INTO students (first_name, last_name, age, grade, course, email, dob, blood_group, country) VALUES ('Jack', 'Kane', 21, 'B', 'Biology', 'alice.jack@example.com', '2004-05-19', 'B+', 'USA');
+
 SELECT  *
 FROM students;
--- SELECT or view a column
-WITH different name 
+-- SELECT or view a column 
+WITH different name
 SELECT  email AS "Student Email"
 FROM students;
 -- See specific column data IN sorted way 
 SELECT  *
 FROM students
-ORDER BY age ASC
+ORDER BY dob DESC
+SELECT  country
+FROM students;
+
+SELECT  DISTINCT country
+FROM students;
+
+SELECT  DISTINCT blood_group
+FROM students;
+-- data filtering 
+-- SELECT students 
+SELECT  * FROM students
+           WHERE country = 'USA';
+
+
+-- Grade and subject as well for student
+
+SELECT * FROM students
+      WHERE grade ='A' AND course='History';
+
+
+
+select upper(first_name), * from students;
+
+select upper(first_name) as first_name_in_upper_case, * from students;
+
+
+select concat(first_name, ' ',last_name) from students;
+
